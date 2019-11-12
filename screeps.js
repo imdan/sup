a4d31bc5bc392b36c48cb35a0aac70175cc27de7
Dan <dmcg926@gmail.com>
2019-11-11 22:28:35 -0600

    nothing is loading...

--- screeps.js
+++ screeps.js
@@ -1,37 +1,22 @@
 let hello = document.querySelector('p'),
     home = document.getElementById('home'),
-    i = 0;
-
-const words = ["Hello world.", "Hola mundo.", "こんにちは世界。", "Bonjour le monde.", "नमस्ते दुनिया।", "Hallo welt.", "你好，世界。", "Pozdrav svijete.", "Dia duit ar domhan.", "Witaj świecie.", "Hej världen.", "Ciao mondo."];
-
+    loading = document.getElementById('loading');
 
 setTimeout(function () {
     hello.style.opacity = '1';
-}, 500);
+    loading.style.opacity = '1';
+}, 500);    
+ 
+function getRandomNum(min, max){
+    return Math.floor((Math.random()*(max-min+1)+min)*10000);
+}
 
-home.addEventListener('click', function (e) {
+setTimeout(function () {
+    hello.style.opacity = '0';
+    loading.style.opacity = '0';
+}, getRandomNum(1,5));
+
+home.addEventListener('click', function(e) {
     e.preventDefault();
     location.reload();
 });
-
-function translate() {
-    setTimeout(function () {
-        hello.style.opacity = '0';
-    }, 3000);
-    
-    setTimeout(function () {
-        if (i < words.length - 1) {
-            i += 1;
-        } else {
-            i = 0;
-        }
-
-        hello.innerHTML = words[i];
-        hello.style.opacity = '1';
-
-        translate();
-    }, 5500);
-    
-}
-
-translate();
