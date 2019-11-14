f1ac658c7781f3bc5927bbc9492f1d56a9bb9ddd
Dan <dmcg926@gmail.com>
2019-11-14 12:35:28 -0600

    blank

--- screeps.js
+++ screeps.js
@@ -1,20 +1,20 @@
-let hello = document.querySelector('p'),
-    home = document.getElementById('home'),
-    loading = document.getElementById('loading');
+let home = document.getElementById('home');
+    // hello = document.querySelector('p'),
+    // loading = document.getElementById('loading');
 
-setTimeout(function () {
-    hello.style.opacity = '1';
-    loading.style.opacity = '1';
-}, 500);    
+// setTimeout(function () {
+//     hello.style.opacity = '1';
+//     loading.style.opacity = '1';
+// }, 500);    
  
-function getRandomNum(min, max){
-    return Math.floor((Math.random()*(max-min+1)+min)*10000);
-}
+// function getRandomNum(min, max){
+//     return Math.floor((Math.random()*(max-min+1)+min)*10000);
+// }
 
-setTimeout(function () {
-    hello.style.opacity = '0';
-    loading.style.opacity = '0';
-}, getRandomNum(1,5));
+// setTimeout(function () {
+//     hello.style.opacity = '0';
+//     loading.style.opacity = '0';
+// }, getRandomNum(1,5));
 
 home.addEventListener('click', function(e) {
     e.preventDefault();
