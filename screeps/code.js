8fd3d0e36468e4ddb38d9e6f87602d43e5959e81
Dan <dmcg926@gmail.com>
2019-11-16 21:11:36 -0600

    code

---
+++ screeps/code.js
@@ -0,0 +1,16 @@
+let codeWords = document.getElementById('code_words'), 
+    morse = document.getElementById('morse'),
+    binary = document.getElementById('binary');
+
+setTimeout(function () {
+    codeWords.style.opacity = '1';
+}, 500);    
+
+setTimeout(function () {
+    morse.style.opacity = '1';
+}, 750);   
+
+setTimeout(function () {
+    binary.style.opacity = '1';
+}, 1000);   
+
