d3101b945df012d0e175e2042dd183fa0cd30860
Dan <dmcg926@gmail.com>
2019-11-18 13:27:56 -0600

    loading and fine

---
+++ screeps/loading.js
@@ -0,0 +1,18 @@
+let loadingWords = document.getElementById('loading_words'), 
+    loading = document.getElementById('loading');
+
+setTimeout(function () {
+    loadingWords.style.opacity = '1';
+    loading.style.opacity = '1';
+}, 500); 
+
+function getRandomNum(min, max){
+    return Math.floor((Math.random()*(max-min+1)+min)*10000);
+}
+
+// console.log(getRandomNum(0,2));
+
+setTimeout(function() {
+    loadingWords.style.opacity = '0';
+    loading.style.opacity = '0';
+}, getRandomNum(0,2));
