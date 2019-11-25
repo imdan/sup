7f7c0616d97cf4458735af5908bf86bc7e3afcbf
Dan <dmcg926@gmail.com>
2019-11-25 01:17:34 -0600

    nevermind

--- screeps/got_it.js
+++
@@ -1,9 +0,0 @@
-let gotIt = document.getElementById('got_it');
-
-setTimeout(function(){
-    gotIt.style.opacity = '1';
-}, 2000);
-
-setTimeout(function() {
-    gotIt.style.opacity = '0';
-}, 5000);
--- screeps/morning.js
+++
@@ -1,5 +0,0 @@
-let morningWords = document.getElementById('morning_words');
-
-setTimeout(function() {
-    morningWords.style.opacity = '1';
-}, 500);
